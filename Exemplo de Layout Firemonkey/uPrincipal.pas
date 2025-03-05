e4de296f787d563cd15e5c030beade1a82f1be0e
Leandro Sartor <leandrosartorp@gmail.com>
2025-03-05 09:03:19 -0300

    Add files via upload

---
+++ Exemplo de Layout Firemonkey/uPrincipal.pas
@@ -0,0 +1,86 @@
+unit uPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Math.Vectors, FMX.ListBox, FMX.Layouts, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Controls3D, FMX.Layers3D,
+  FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Lay: TLayout;
+    Layout3D1: TLayout3D;
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Layout3: TLayout;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+    Rectangle3: TRectangle;
+    Layout4: TLayout;
+    Label1: TLabel;
+    Label2: TLabel;
+    Image1: TImage;
+    Label3: TLabel;
+    Label4: TLabel;
+    Layout5: TLayout;
+    Layout6: TLayout;
+    Rectangle4: TRectangle;
+    ListBox1: TListBox;
+    ListBoxItem1: TListBoxItem;
+    ListBoxItem2: TListBoxItem;
+    ListBoxItem3: TListBoxItem;
+    ListBoxItem4: TListBoxItem;
+    ListBoxItem5: TListBoxItem;
+    ListBoxItem6: TListBoxItem;
+    ListBoxItem7: TListBoxItem;
+    ListBoxItem8: TListBoxItem;
+    ListBoxItem9: TListBoxItem;
+    ListBoxItem10: TListBoxItem;
+    StyleBook1: TStyleBook;
+    Layout7: TLayout;
+    Layout8: TLayout;
+    Layout9: TLayout;
+    Rectangle5: TRectangle;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Layout10: TLayout;
+    Label9: TLabel;
+    Edit1: TEdit;
+    SearchEditButton1: TSearchEditButton;
+    Layout11: TLayout;
+    Layout12: TLayout;
+    Layout13: TLayout;
+    Rectangle8: TRectangle;
+    Rectangle9: TRectangle;
+    ListBox2: TListBox;
+    ListBoxItem11: TListBoxItem;
+    ListBoxItem12: TListBoxItem;
+    ListBoxItem13: TListBoxItem;
+    ListBoxItem14: TListBoxItem;
+    ListBoxItem15: TListBoxItem;
+    ListBoxItem16: TListBoxItem;
+    ListBoxItem17: TListBoxItem;
+    ListBoxItem18: TListBoxItem;
+    ListBoxItem19: TListBoxItem;
+    ListBoxItem20: TListBoxItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
